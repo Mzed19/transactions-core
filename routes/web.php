a21a9e975ff78f9bafdde6cf80189dd7542888dd
Michael Dumont <74337976+Mzed19@users.noreply.github.com>
2026-01-26 23:50:00 -0300

    Add route to download Postman collection JSON file
    - Implemented a new route '/download/postman-collection' to allow users to download the Postman collection file.
    - Added error handling to return a 404 response if the file does not exist.
    - Set the appropriate content type for the downloaded file.

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/download/postman-collection', function () {
+    $filePath = base_path('Transactions Core.postman_collection.json');
+    
+    if (!file_exists($filePath)) {
+        abort(404, 'Arquivo não encontrado');
+    }
+    
+    return response()->download($filePath, 'Transactions Core.postman_collection.json', [
+        'Content-Type' => 'application/json',
+    ]);
+})->name('postman.download');
